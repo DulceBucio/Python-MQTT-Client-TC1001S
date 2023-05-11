56872ed40dd6e5145212b574bf24796c7b1a2e80
DulceBucio <dulcenahomibucio@gmail.com>
2023-05-11 17:45:33 -0600

    Receive messages function

--- main.py
+++ main.py
@@ -26,3 +26,24 @@ def descifrar_orden(argv):
         return[broker,topic]
 
 
+# Function to publish a message in a topic
+def publish(client, topic, msg):
+    msg_count = 0
+    while True:
+        result = client.publish(topic, msg)
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+            break
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+# Function to suscribe the client and receive messages
+def subscribe(client: mqtt_client, topic):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
